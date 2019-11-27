6d477904b70132b288bcbf81f813c89a681dcafa
khalidnouh <khalid.nouh@intellisc.com>
2019-11-27 09:14:26 +0200

    Repeated String At Hacker Rank

--- Java/src/JavaStack/Solution.java
+++ Java/src/JavaStack/Solution.java
@@ -13,51 +13,48 @@ import java.util.Stack;
  * @author Khalid.nouh
  */
 
-public class Solution {	
-	public static void main(String[] argh) {
-
-		Scanner sc = new Scanner(System.in);
-		boolean isBalanced = true;
-		Stack<Character> myStack = new Stack<>();
-		while (sc.hasNext()) {		
-			myStack=new Stack<>();
-			String input = sc.next();
-			 if((input == null) || (input.trim().isEmpty())) {
-				 System.err.println("emp");
-			 }
-
-			char[] values = input.toCharArray();
-			for (char c : values) {
-				if (c == '(' || c == '[' || c == '{') {
-					myStack.push(c);
-				}					
-			else if(myStack.size()>=1){
-					if (c == '}') {
-						c = '{';
-						if (myStack.peek() == c) {
-							myStack.pop();
-						}
-					} else if (c == ')') {
-						c = '(';
-						if (myStack.peek() == c) {
-							myStack.pop();
-						}
-					} else if (c == ']') {
-						c = '[';
-						if (myStack.peek() == c) {
-							myStack.pop();
-						}
+public class Solution {
+	
+	private static boolean isBalancedString(String input) {
+		Stack<Character> myStack = new Stack<Character>();
+		for (char ch : input.toCharArray()) {
+			if (ch == '(' || ch == '[' || ch == '{') {
+				myStack.push(ch);
+			}
+			if (ch == ')' || ch == ']' || ch == '}') {
+				if (myStack.empty()) {
+					return false;
+				}
+				if (ch == ')') {
+					if (myStack.peek() == '(') {
+						myStack.pop();
+					} else {
+						return false;
 					}
 				}
-						
+				if (ch == ']') {
+					if (myStack.peek() == '[') {
+						myStack.pop();
+					} else {
+						return false;
+					}
+				}
+				if (ch == '}') {
+					if (myStack.peek() == '{') {
+						myStack.pop();
+					} else {
+						return false;
+					}
+				}
 			}
-			if (myStack.isEmpty())
-				isBalanced = true;
-			else
-				isBalanced = false;
-			System.out.println(isBalanced ? true : false);
-	
 		}
-
+		return myStack.empty();
+	}
+	public static void main(String[] argh) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			String input = sc.next();
+			System.out.println(Solution.isBalancedString(input));
+		}
 	}
 }
